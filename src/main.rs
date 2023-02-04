7b08b4cbc49ab5191f9c50474e3d003407d803d9
Jett Thompson <jett.thompson.59@gmail.com>
2023-02-04 16:32:33 +1100

    add example source file and command line parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use clap::Parser;
+
+#[derive(Debug, clap::Parser)]
+struct CommandLineOptions {
+    /// The amount of optimization to perform on the code;
+    /// 0 = no optimizations,
+    /// 1 = some optimizations,
+    /// 2 = most optimizations,
+    /// 3 = aggressive optimizations
+    #[clap(short = 'O', default_value = "2")]
+    optimization_level: i32,
+    #[clap(short, long = "output")]
+    output_file: String,
+
+    input_file: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let options = CommandLineOptions::parse();
+    println!("{:?}", options)
 }
