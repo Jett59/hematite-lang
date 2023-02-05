7532bf7830ea0cbdbd98c599674c81cfe3eb4ed8
Jett Thompson <jett.thompson.59@gmail.com>
2023-02-05 12:10:00 +1100

    add the ability to parse identifiers and make errors not continue to go on forever

--- src/lexer.rs
+++ src/lexer.rs
@@ -39,22 +39,59 @@ trait TokenParser {
     fn complete(&self) -> Option<Token>;
 }
 
+struct IdentifierParser {
+    so_far: String,
+}
+
+impl IdentifierParser {
+    fn new() -> Self {
+        Self {
+            so_far: String::new(),
+        }
+    }
+}
+
+impl TokenParser for IdentifierParser {
+    fn accept(&self, character: char) -> Option<Box<dyn TokenParser>> {
+        if character.is_alphanumeric() || character == '_' {
+            Some(Box::new(IdentifierParser {
+                so_far: format!("{}{}", self.so_far, character),
+            }))
+        } else {
+            None
+        }
+    }
+    fn complete(&self) -> Option<Token> {
+        Some(Token::Identifier(self.so_far.clone()))
+    }
+}
+
 helper_macros::exact_match_token! {Arrow: "->"}
 
 pub struct TokenIterator<'base_iterator> {
     base_iterator: Peekable<&'base_iterator mut dyn Iterator<Item = char>>,
+    found_invalid_token: bool,
 }
 
 impl Iterator for TokenIterator<'_> {
     type Item = Token;
 
     fn next(&mut self) -> Option<Self::Item> {
-        while self.base_iterator.peek().is_some()
-            && self.base_iterator.peek().unwrap().is_whitespace()
+        if self.found_invalid_token {
+            return None;
+        }
+        while self
+            .base_iterator
+            .peek()
+            .filter(|character| character.is_whitespace())
+            .is_some()
         {
             self.base_iterator.next();
         }
-        let mut possibilities: Vec<Box<dyn TokenParser>> = vec![Box::new(ArrowParser::new())];
+        let mut possibilities: Vec<Box<dyn TokenParser>> = vec![
+            Box::new(IdentifierParser::new()),
+            Box::new(ArrowParser::new()),
+        ];
         let mut characters_read_so_far = String::new();
         while let Some(next_character) = self.base_iterator.peek() {
             let new_possibilities = possibilities
@@ -63,6 +100,12 @@ impl Iterator for TokenIterator<'_> {
                 .collect::<Vec<_>>();
             if new_possibilities.is_empty() {
                 // This means that we have read a complete token or the input is invalid.
+                if characters_read_so_far.is_empty() {
+                    self.found_invalid_token = true;
+                    return Some(Token::Error(
+                        format!("Invalid character: {next_character}",),
+                    ));
+                }
                 let mut completed_tokens = possibilities
                     .iter()
                     .filter_map(|possibility| possibility.complete());
@@ -71,6 +114,7 @@ impl Iterator for TokenIterator<'_> {
                 if let Some(completed_token) = completed_tokens.next() {
                     return Some(completed_token);
                 } else {
+                    self.found_invalid_token = true;
                     return Some(Token::Error(format!(
                         "Invalid token: {characters_read_so_far}{next_character}",
                     )));
@@ -88,5 +132,6 @@ impl Iterator for TokenIterator<'_> {
 pub fn tokenize(input: &mut dyn Iterator<Item = char>) -> TokenIterator {
     TokenIterator {
         base_iterator: input.peekable(),
+        found_invalid_token: false,
     }
 }
